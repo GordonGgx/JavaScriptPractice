7382a60bfe42b91222b3fe7e7115c5ad54e64881
guanguoxiang <abcgg8@qq.com>
2017-06-27 01:11:51 +0800

    array

---
+++ array.js
@@ -0,0 +1,9 @@
+//数组
+var empty=[];//没有元素的数组
+var primes=[1,3,5,7,9];//创建5个数值的数组
+var misc=[1.1,true,"a",];//创建了三个不同元素的数组以逗号结尾
+console.log("mics.length="+misc.length);//输出3
+console.log("mics[3]="+misc[3]);
+var list=[,,];
+console.log("list长度="+list.length);
+console.log(list[0]);
